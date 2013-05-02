7f7d87b9ad99b6e43441f283f62d3c2d3f246afd
multisnow <infinity.blick.winkel@gmail.com>
2013-05-02 23:11:36 +0800

    bigger convas icon

--- js/function.canvas.js
+++ js/function.canvas.js
@@ -19,23 +19,23 @@
 
 function pausecanvas(node){
     var icon=node.getContext('2d');
-    node.width=16;
-    node.height=16;
+    node.width=24;
+    node.height=24;
     icon.fillStyle='#ffff00';
-    icon.fillRect(0,0,6.5,16);
-    icon.fillRect(9.5,0,16,16);
+    icon.fillRect(0,0,10,24);
+    icon.fillRect(14,0,24,24);
     return 0;
 };
 
 function unpausecanvas(node){
     var icon=node.getContext('2d')
-    node.width=16;
-    node.height=16;
+    node.width=24;
+    node.height=24;
     icon.fillStyle='#00ff00';
     icon.beginPath();
-    icon.moveTo(1,1);
-    icon.lineTo(1,15);
-    icon.lineTo(15,8);
+    icon.moveTo(0,1);
+    icon.lineTo(0,23);
+    icon.lineTo(23,12);
     icon.closePath();
     icon.fill();
     return 0;
@@ -43,33 +43,31 @@ function unpausecanvas(node){
 
 function removecanvas(node){
     var icon=node.getContext('2d');
-    node.width=16;
-    node.height=16;
+    node.width=24;
+    node.height=24;
     icon.fillStyle='#ff0000';
-    icon.setTransform(1,1,-1,1,8,-8);
-    icon.fillRect(0,6,16,4);
-    icon.fillRect(6,0,4,16);
+    icon.fillRect(0,0,24,24);
+    icon.globalCompositeOperation='destination-out';
+    icon.strokeRect(4,4,16,16);
     return 0;
 };
 
 function optioncanvas(node){
     var icon=node.getContext('2d');
-    node.width=16;
-    node.height=16;
+    node.width=24;
+    node.height=24;
     icon.fillStyle='#a0a0a0';
-    icon.fillStyle='#ff0000';
-    icon.fillStyle='#a0a0a0';
-    icon.translate(8,8);
-    icon.beginPath();
-    icon.arc(0,0,6,0,2*Math.PI);
+    icon.translate(12,12);
+    icon.arc(0,0,10,0,2*Math.PI);
     icon.fill();
     for(var i=0;i<12;i++){
         icon.rotate(Math.PI/6);
-        icon.fillRect(0,-1,8,2);
+        icon.fillRect(0,-1.5,12,3);
     };
     icon.beginPath();
     icon.globalCompositeOperation='destination-out';
-    icon.arc(0,0,3,0,2*Math.PI);
+    icon.arc(0,0,6,0,2*Math.PI);
+    icon.closePath();
     icon.fill();
     return 0;
 };
